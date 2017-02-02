83237e3ed536130b3f6aa8865642ab24cb507826
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-02 13:24:57 -0600

    moved stuff to main partial

--- app/app.js
+++ app/app.js
@@ -2,34 +2,28 @@ const app = angular.module('musicHistory', ['ngRoute'])
 
 app
   .config(($routeProvider) => {
-    routeProvider
+    $routeProvider
       .when('/', {
         controller: 'loginCtrl',
-        templateUrl: '/app/partials/login.html',
-        resolve: ,
+        templateUrl: '/app/partials/login.html'
+        // resolve:
       })
       .when('/register', {
         controller: 'registerCtrl',
-        templateUrl: '/app/partials/register.html',
-        resolve: ,
+        templateUrl: '/app/partials/register.html'
+        // resolve:
       })
       .when('/main', {
         controller: 'mainCtrl',
-        templateUrl: '/app/partials/main.html',
-        resolve: ,
+        templateUrl: '/app/partials/main.html'
+        // resolve:
       })
       .when('/add', {
         controller: 'addCtrl',
-        templateUrl: '/app/partials/add.html',
-        resolve: ,
+        templateUrl: '/app/partials/add.html'
+        // resolve:
       })
       .otherwise({
         redirectTo: ('/')
       })
-  })
-  .directive('playlist', function () {
-    return {
-      restrict: 'E',
-      templateUrl: "app/partials/playlist.html"
-    }
   })
