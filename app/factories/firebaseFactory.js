8a75948bb969aef7fe99430b981772a9a7d24cb7
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-02 13:58:39 -0600

    introduced addTrack and deleteTrack

--- app/factories/firebaseFactory.js
+++ app/factories/firebaseFactory.js
@@ -2,13 +2,21 @@ app
   .factory('firebaseFactory', ($http) => {
     return {
     // get songs from fireBase
-      getSongs : () => {
+      getTracks : () => {
         return $http
           .get('https://freakin-music-history.firebaseio.com/.json')
           .then((res) => {
             return res;
             console.log(res);
           })
+      },
+      addTrack : () => {
+        return $http
+          .post('https://freakin-music-history.firebaseio.com/.json')
+      },
+      deleteTrack : (track) => {
+        return $http
+          .delete(`https://freakin-music-history.firebaseio.com/${track}.json`)
       }
 
     }
