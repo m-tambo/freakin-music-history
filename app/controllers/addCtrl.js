5a3167338d95d666f2131ee1de47814e8dad5f87
Michael Tambornino <michael.tambornino@gmail.com>
2017-02-03 11:02:19 -0600

    fixed postTrack function

--- app/controllers/addCtrl.js
+++ app/controllers/addCtrl.js
@@ -1,8 +1,16 @@
 app.controller('addCtrl', function(firebaseFactory, $scope) {
 
   $scope.addTrack = function () {
-    console.log('fire addTrack function')
-    firebaseFactory.postTrack(title, artist, album)
+
+    firebaseFactory.postTrack($scope.trackTitle, $scope.trackArtist, $scope.trackAlbum)
+      .then(() => { // reset form
+        $scope.trackTitle = '';
+        $scope.trackArtist = '';
+        $scope.trackAlbum = '';
+      })
+      .then(() => {
+        redirectTo: ('/main')
+      })
   }
 
 })
